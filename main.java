43269d9934e2bc2a1a2379cab38a62c266260837
Hawkeye <realisvenator@gmail.com>
2020-02-07 18:47:44 -0600

    Added to supplier class
    Added all of the setters,getters and to string tothe supplier class and added all of the the variables I think we will need for now.

---
+++ main.java
@@ -0,0 +1,8 @@
+package Project1;
+public class main{
+    public static void main(String args[]){
+        Product p1 = new Product("test product","test description",34,4,32.32,2234);
+        System.out.println(p1);
+
+    }
+}
