ec2034a250feb46235feabd9e41c5bf34e74816a
anjalisuwal <43836750+anjalisuwal@users.noreply.github.com>
2020-02-29 23:09:58 -0600

    Add files via upload

---
+++ shoppingcart.java
@@ -0,0 +1,33 @@
+import java.text.DecimalFormat;
+import java.util.*;
+import java.io.*;
+
+
+class ShopiingCart implements Serializable {
+    private Product item;
+    private int quantity;
+        
+    ShopiingCart(Product item, int quantity){
+        this.item = item;
+        this.quantity = quantity;
+    }//end constructor
+        
+    public Product getItem(){
+        return item;
+    }//end getItem
+
+    public int getQuantity(){
+        return quantity;
+    }//end getQuantity
+        
+    public void setItem(Product item){
+        this.item = item;
+    }//end setItem
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }//end setId
+        
+    public String toString(){
+        return "Name: " + item.getName() + " Quantity: " + quantity;
+    }//end display
